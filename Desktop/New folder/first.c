35d04aca8fdc9b5d635cd5cce670a64b1abf693d
krishna dubey <krishnadubey1008@gmail.com>
2023-08-12 12:33:41 +0530

    jnjd

---
+++ Desktop/New folder/first.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    printf("ram ram");
+    printf("%d",8);
+    return 0;
+    }
---
+++ Desktop/c++course/tut3.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+//this program was created by code with harry
+int main(){
+    cout<<"HELLO WORLD";
+    return 0;
+}
